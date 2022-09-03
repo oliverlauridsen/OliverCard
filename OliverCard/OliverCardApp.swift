78fca1f28c14dfaa6b30e17d4f66e80fbb6e1b7f
Oliver Lauridsen <olidude2.0@gmail.com>
2022-09-03 15:03:49 +0200

    Initial Commit

---
+++ OliverCard/OliverCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OliverCardApp.swift
+//  OliverCard
+//
+//  Created by Oliver Lauridsen on 03/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct OliverCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
